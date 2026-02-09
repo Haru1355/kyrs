7ad4fbf0205eef238df03759bcf9b7db337784cd
Haru1355 <Samodurova_marysia@mail.ru>
2026-02-10 00:17:49 +0300

    g

--- front/src/componens/heder/heder.tsx
+++ front/src/componens/heder/heder.tsx
@@ -1,23 +1,35 @@
-import { Link } from 'react-router'
+import { Link } from 'react-router';
 
 export const Header = () => {
-	return <header className='flex max-w-[1400px] mx-auto justify-between items-center px-5 my-5 gap-12'> 
-		<div className="w-full ">
-			<ul className='flex gap-2 justify-between'>
-			<li><Link to="">О компании </Link></li>
-			<li><Link to="">Бренды</Link></li>
-			<li><Link to="">продукция </Link></li>
-			<li><Link to="/news">новости </Link></li>
-			</ul>
-		</div>
-		<img className="w-full max-w-[157px] h-[141px]" src="/src/assets/logo.svg" alt="logo" />
-		<div className="w-full">
-			<ul className='flex gap-2 justify-between'>
-			<li><Link to="">Благотворительны фонд </Link></li>
-			<li><Link to="">Пресса</Link></li>
-			<li><Link to="">Доставка</Link></li>
-			<li><Link to="">контакты </Link></li>
-			</ul>
-		</div>
-	</header>;
-}
+	return (
+		<header className='flex max-w-[1400px] mx-auto justify-between items-center px-5 my-5 gap-12'>
+			<img
+				className='w-full max-w-[157px] h-[141px]'
+				src='/src/assets/logo.svg'
+				alt='logo'
+			/>
+			<div className='w-full text-gray-600'>
+				<ul className='flex gap-2 justify-between'>
+					<li>
+						<Link to=''>О компании </Link>
+					</li>
+					<li>
+						<Link to=''>Магазины CU</Link>
+					</li>
+					<li>
+						<Link to=''>Продукция </Link>
+					</li>
+					<li>
+						<Link to='/news'>Новости </Link>
+					</li>
+					<li>
+						<Link to=''>Доставка</Link>
+					</li>
+					<li>
+						<Link to=''>Контакты </Link>
+					</li>
+				</ul>
+			</div>
+		</header>
+	);
+};

46beda92d2d58b36b3d1076f0402451697d960eb
Haru1355 <Samodurova_marysia@mail.ru>
2026-02-15 03:24:54 +0300

    d

--- front/src/componens/heder/heder.tsx
+++ front/src/componens/heder/heder.tsx
@@ -8,25 +8,28 @@ export const Header = () => {
 				src='/src/assets/logo.svg'
 				alt='logo'
 			/>
-			<div className='w-full text-gray-600'>
+			<div className=' w-full justify-center text-gray-600'>
 				<ul className='flex gap-2 justify-between'>
 					<li>
-						<Link to=''>О компании </Link>
+						<Link to=''>Продукты </Link>
 					</li>
 					<li>
 						<Link to=''>Магазины CU</Link>
 					</li>
 					<li>
-						<Link to=''>Продукция </Link>
+						<Link to=''>О компании </Link>
 					</li>
 					<li>
 						<Link to='/news'>Новости </Link>
 					</li>
 					<li>
-						<Link to=''>Доставка</Link>
+						<Link to="">Карьера </Link>
 					</li>
 					<li>
 						<Link to=''>Контакты </Link>
+					</li>
+					<li>
+						<Link to=''>Доставка Алматы </Link>
 					</li>
 				</ul>
 			</div>

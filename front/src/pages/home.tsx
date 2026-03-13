e2112ef7547c5927b2bbf0a0e232696dc855c690
Haru1355 <Samodurova_marysia@mail.ru>
2026-03-14 02:33:47 +0300

    aaa

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router'
-import cn from "classnames";
-import { useState } from "react";
+import cn from 'classnames';
+import { useState } from 'react';
+import { Link } from 'react-router';
 
 export const Home = () => {
 	const [activeIndex, setActiveIndex] = useState(0);
@@ -9,102 +9,143 @@ export const Home = () => {
 	const cards = [
 		{
 			id: 0,
-			title: "Мороженное",
-			description: "c 1995 года",
-			image: "/src/assets/main/hovered-items/icecream.png"
+			title: 'Мороженное ',
+			p: 'Крупнейшее производство в Центральной Азии: инновационные технологии и 163 вида мороженого',
+			description: 'c 1995 года',
+			image: '/src/assets/main/hovered-items/icecream.png',
 		},
 		{
 			id: 1,
-			title: "Замороженные полуфабрикаты",
-			description: "c 2009 года",
-			image: "/src/assets/main/hovered-items/polyfab.png"
+			title: 'Замороженные полуфабрикаты',
+			description: 'c 2009 года',
+			image: '/src/assets/main/hovered-items/polyfab.png',
 		},
 		{
 			id: 2,
-			title: "Лапша быстрого приготовления",
-			description: "c 2012 года",
-			image: "/src/assets/main/hovered-items/lapsha.png"
+			title: 'Лапша быстрого приготовления',
+			description: 'c 2012 года',
+			image: '/src/assets/main/hovered-items/lapsha.png',
 		},
 		{
 			id: 3,
-			title: "Розничная сеть CU в Казахстане",
-			description: "c 2024 года",
-			image: "/src/assets/main/hovered-items/CU.png"
-		}
+			title: 'Розничная сеть CU в Казахстане',
+			description: 'c 2024 года',
+			image: '/src/assets/main/hovered-items/CU.png',
+		},
 	];
 
 	return (
-			<div className='container mx-auto mt-[-40px] '>
-				<img src='/src/assets/main/1main.webp' alt='' />
-				<div className=''>
-					<img src='/src/assets/main/2main.webp' alt='' />
+		<div className='container mx-auto mt-[-40px] relative '>
+			<img src='/src/assets/main/1main.webp' alt='' />
+			<div className='absolute inset-0 top-[3%] left-[5%]'>
+				<img src='/src/assets/main/2main.svg' width={600} height={500} alt='' />
+			</div>
+			<div className='flex pb-[70px] mt-[-90px] rounded-t-[90px] bg-white transform-3d'>
+				<div className='w-1/2 pt-20 pl-[110px] pr-20 '>
+					<h1 className='md font-[cursive] font-bold text-[50px]'>
+						Группа компаний Шин-Лайн
+					</h1>
+					<p className='font-[cursive] text-2xl'>
+						Основанная в 1995 году как семейное предприятие, сегодня компания
+						стала №1 по производству мороженого в Центральной Азии. В 2025 году
+						мы выпустили 37 000 тонн продукции, соответствующей международным
+						стандартам качества.
+					</p>
+					<Link to='' className='text-xl bg-[#a49a9a67] rounded-full px-10 relative'>
+						Подробнее <img className='absolute ' src='/src/assets/arrow.svg' />
+					</Link>
+					
 				</div>
-				<div className='flex pb-[70px] mt-[-90px] rounded-t-[90px] bg-white transform-3d'>
-					<div className='w-1/2 pt-20 pl-[110px] pr-20 '>
-						<h1 className='md font-[cursive] font-bold text-[50px]'>Группа компаний Шин-Лайн</h1>
-						<p className='font-[cursive] text-xl'>
-							Основанная в 1995 году как семейное предприятие, сегодня компания
-							стала №1 по производству мороженого в Центральной Азии. В 2025 году
-							мы выпустили 37 000 тонн продукции, соответствующей международным
-							стандартам качества.
-						</p>
-						<Link to='' className='text-xl '>
-							Подробнее <img className='' src='/src/assets/arrow.svg' />
-						</Link>
-					</div>
-					<div className='w-1/2 pt-20 pb-[150px] pl-20 pr-[140px]  bg-[#e4e9f0] rounded-t-[90px] '>
-						<ul>
-							<li className='-rotate-10 flex  flex-wrap items-center gap-x-[30px] leading-none'>
-								<span className='text-[#e50909] text-[100px] font-extrabold'>10</span>
-								<span className='max-w-[220px] text-[26px] font-semibold'>Стран импортеров</span>
-							</li>
-							<li className='-rotate-10 flex  flex-wrap items-center gap-x-[30px] leading-none'>
-								<span className='text-[#e50909] text-[100px] font-extrabold'>16</span>
-								<span className='max-w-[220px] text-[26px] font-semibold'>Филиалов</span>
-							</li>
-							<li className='-rotate-10 flex flex-wrap items-center gap-x-[30px] leading-none'>
-								<span className='text-[#e50909] text-[100px] font-extrabold'>100</span>
-								<span className='max-w-[220px] text-[26px] font-semibold'>Дистрибьюторов</span>
-							</li>
-							<li className='-rotate-10 flex flex-wrap items-center gap-x-[30px] leading-none'>
-								<span className='text-[#e50909] text-[100px] font-extrabold'>130.000</span>
-								<span  className=' text-[26px] font-semibold text-end w-2/3'>Точек продаж</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div className='mt-[-150px] transform-3d bg-white pt-[90px] rounded-t-[90px] pb-20'>
-					<h2 className='text-[30px] pl-20 -mt-10'>Направления деятельности</h2>
-					<div className='flex mt-10 px-20 gap-x-[20px] max-w-[1800px] w-full'>
-						{cards.map((card) => (
-								<div
-										key={card.id}
-										className={cn(
-												'relative flex flex-col grow gap-y-5 h-[480px] justify-between pt-[45px] px-5 pb-[25px] transition-all duration-300',
-												{
-													'!w-[480px] aspect-square': activeIndex === card.id,
-													'w-[calc((100%-630px)/3)]': activeIndex !== card.id,
-												}
-										)}
-										onMouseEnter={() => setActiveIndex(card.id)}
-								>
-									<div className='absolute w-full h-full inset-0'>
-										<img
-												src={card.image}
-												alt=''
-												className={cn('object-bottom object-cover w-full h-full rounded-[20px] transition-all duration-300', {
-													'rounded-[500px]': activeIndex === card.id,
-												})}
-										/>
-									</div>
-									<div className='relative z-10 max-w-[330px] mx-auto text-center'>
-										<p>{card.description}</p>
-										<h4>{card.title}</h4>
-									</div>
-								</div>
-						))}
-					</div>
+				<div className='w-1/2 pt-20 pb-[150px] pl-20 pr-[140px]  bg-[#e4e9f0] rounded-t-[90px] '>
+					<ul>
+						<li className='-rotate-10 flex  flex-wrap items-center gap-x-[30px] leading-none'>
+							<span className='text-[#e50909] text-[100px] font-extrabold'>
+								10
+							</span>
+							<span className='max-w-[220px] text-[26px] font-semibold'>
+								Стран импортеров
+							</span>
+						</li>
+						<li className='-rotate-10 flex  flex-wrap items-center gap-x-[30px] leading-none'>
+							<span className='text-[#e50909] text-[100px] font-extrabold'>
+								16
+							</span>
+							<span className='max-w-[220px] text-[26px] font-semibold'>
+								Филиалов
+							</span>
+						</li>
+						<li className='-rotate-10 flex flex-wrap items-center gap-x-[30px] leading-none'>
+							<span className='text-[#e50909] text-[100px] font-extrabold'>
+								100
+							</span>
+							<span className='max-w-[220px] text-[26px] font-semibold'>
+								Дистрибьюторов
+							</span>
+						</li>
+						<li className='-rotate-10 flex flex-wrap items-center gap-x-[30px] leading-none'>
+							<span className='text-[#e50909] text-[100px] font-extrabold'>
+								130.000
+							</span>
+							<span className=' text-[26px] font-semibold text-end w-2/3'>
+								Точек продаж
+							</span>
+						</li>
+					</ul>
 				</div>
 			</div>
+			<div className='flex pb-[70px] mt-[-90px] relative'>
+				<img
+					src='\src\assets\main\3main.webp'
+					alt=''
+					className=' rounded-t-[90px] transform-3d'
+				/>
+				<div className='w-1/2 pt-20 pl-[110px] pr-20 absolute'>
+					<h1 className='md font-[cursive] font-bold text-[50px] text-white'>
+						Крупнейший индустриальный комплекс
+					</h1>
+					<h3>37 000 тонн производства в 2025 году </h3>
+				</div>
+			</div>
+			<div className='mt-[-150px] transform-3d bg-white pt-[90px] rounded-t-[90px] pb-20'>
+				<h2 className='font-[cursive]  text-[40px] text-center pl-20 -mt-10'>
+					Направления деятельности
+				</h2>
+				<div className='flex mt-10 px-20 gap-x-[20px] max-w-[1800px] w-full font-[cursive] text-fuchsia-50'>
+					{cards.map(card => (
+						<div
+							key={card.id}
+							className={cn(
+								'relative flex flex-col grow gap-y-5 h-[480px] justify-between pt-[45px] px-5 pb-[25px] transition-all duration-300',
+								{
+									'!w-[480px] aspect-square': activeIndex === card.id,
+									'w-[calc((100%-630px)/3)]': activeIndex !== card.id,
+								},
+							)}
+							onMouseEnter={() => setActiveIndex(card.id)}
+						>
+							<div className='absolute w-full h-full inset-0'>
+								<img
+									src={card.image}
+									alt=''
+									className={cn(
+										'object-bottom object-cover w-full h-full rounded-[20px] transition-all duration-300',
+										{
+											'rounded-[500px]': activeIndex === card.id,
+										},
+									)}
+								/>
+							</div>
+							<div className='relative z-10 max-w-[330px] mx-auto text-center'>
+								<p>{card.description}</p>
+								<h1 className='text-[20px]'>
+									<b>{card.title}</b>
+								</h1>
+								<p>{card.p}</p>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
 	);
 };
--- front/src/pages/products.tsx
+++ front/src/pages/products.tsx
@@ -1,7 +1,8 @@
-import {useEffect, useState, useMemo} from "react";
-import { fetchProducts} from "../api/api.ts";
-import type {ProductsResponse} from "../types/products.ts";
-import {ProductItem} from "../componens/product-item/product-item.tsx";
+import { useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router';
+import { fetchProducts } from '../api/api.ts';
+import { ProductItem } from '../componens/product-item/product-item.tsx';
+import type { ProductsResponse } from '../types/products.ts';
 
 export const ProductsPage = () => {
 	const [products, setProducts] = useState<ProductsResponse | null>(null);
@@ -29,16 +30,19 @@ export const ProductsPage = () => {
 	const productsByBrand = useMemo(() => {
 		if (!products?.data) return {};
 
-		return products.data.reduce((acc, product) => {
-			const brandName = product.brand.name;
+		return products.data.reduce(
+			(acc, product) => {
+				const brandName = product.brand.name;
 
-			if (!acc[brandName]) {
-				acc[brandName] = [];
-			}
+				if (!acc[brandName]) {
+					acc[brandName] = [];
+				}
 
-			acc[brandName].push(product);
-			return acc;
-		}, {} as Record<string, typeof products.data>);
+				acc[brandName].push(product);
+				return acc;
+			},
+			{} as Record<string, typeof products.data>,
+		);
 	}, [products]);
 
 	if (loading) {
@@ -50,22 +54,53 @@ export const ProductsPage = () => {
 	}
 
 	return (
-			<div className='container mx-auto px-20 mb-32'>
+		<div className='container mx-auto -mt-5 '>
+			<div className='relative'>
+				<img src='/src/assets/product/prod.webp' alt='' className='w-full' />
+				<div className='absolute inset-0 flex flex-col  w-1/2 justify-center text-white ml-15 mb-25'>
+					<Link to='' className='font-[cursive] text-xl '>
+						Главная|Мороженное <img className='' src='' />
+					</Link>
+					<h1 className='md font-[cursive] font-bold text-[60px]'>
+						Шин-Лайн — мороженое № 1 в&nbsp;Центральной Азии*
+					</h1>
+					<p className='font-[cursive] text-2xl'>
+						и один из крупнейших производителей в СНГ и Восточной Европе.
+					</p>
+				</div>
+			</div>
+			<div className='flex pb-[70px] mt-[-90px] rounded-t-[90px] bg-[#e4e9f0] transform-3d'>
+				<div className=' pt-10 pl-[520px] '>
+					<h2 className='md font-[cursive] font-bold text-[40px] text-[#384052] '>
+						Наши бренды
+					</h2>
+					<div className="slide">
+						<div className="">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className=' mx-auto px-20 mb-32'>
 				<h1 className='text-[74px] font-extrabold'>Мороженное</h1>
 				{Object.entries(productsByBrand).map(([brandName, brandProducts]) => (
-						<div key={brandName}>
-							<h2 className='text-[52px] font-extrabold my-5'>{brandName}</h2>
-							<ul className='grid grid-cols-4 gap-5'>
-								<li>
-									<img src={`http://localhost:1337${brandProducts[0].brand.logo.url}`} alt=''/>
-									<p>{brandProducts[0].brand.description}</p>
-								</li>
-								{brandProducts.map(product => (
-										<ProductItem key={product.id} product={product} />
-								))}
-							</ul>
-						</div>
+					<div key={brandName}>
+						<h2 className='text-[52px] font-extrabold my-5'>{brandName}</h2>
+						<ul className='grid grid-cols-4 gap-5'>
+							<li>
+								<img
+									src={`http://localhost:1337${brandProducts[0].brand.logo.url}`}
+									alt=''
+								/>
+								<p>{brandProducts[0].brand.description}</p>
+							</li>
+							{brandProducts.map(product => (
+								<ProductItem key={product.id} product={product} />
+							))}
+						</ul>
+					</div>
 				))}
 			</div>
+		</div>
 	);
 };

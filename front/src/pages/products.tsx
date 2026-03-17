21437050b0bb55bc64f136b33be953001edd3235
Haru1355 <Samodurova_marysia@mail.ru>
2026-03-17 04:03:29 +0300

    gg

--- front/src/pages/products.tsx
+++ front/src/pages/products.tsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router';
 import { fetchProducts } from '../api/api.ts';
 import { ProductItem } from '../componens/product-item/product-item.tsx';
+import { Autoplay, Swiper, SwiperSlide } from '../swiper';
 import type { ProductsResponse } from '../types/products.ts';
 
 export const ProductsPage = () => {
@@ -59,7 +60,8 @@ export const ProductsPage = () => {
 				<img src='/src/assets/product/prod.webp' alt='' className='w-full' />
 				<div className='absolute inset-0 flex flex-col  w-1/2 justify-center text-white ml-15 mb-25'>
 					<Link to='' className='font-[cursive] text-xl '>
-						Главная|Мороженное <img className='' src='' />
+						Главная|Мороженное{' '}
+						<img className='' src='http://localhost:5173	' alt='' />
 					</Link>
 					<h1 className='md font-[cursive] font-bold text-[60px]'>
 						Шин-Лайн — мороженое № 1 в&nbsp;Центральной Азии*
@@ -70,14 +72,35 @@ export const ProductsPage = () => {
 				</div>
 			</div>
 			<div className='flex pb-[70px] mt-[-90px] rounded-t-[90px] bg-[#e4e9f0] transform-3d'>
-				<div className=' pt-10 pl-[520px] '>
-					<h2 className='md font-[cursive] font-bold text-[40px] text-[#384052] '>
+				<div className='w-full pt-10 px-20'>
+					<h2 className='md font-[cursive] font-bold text-[40px] text-[#384052] text-center'>
 						Наши бренды
 					</h2>
-					<div className="slide">
-						<div className="">
-							
-						</div>
+					<div className='slide'>
+						<Swiper
+							modules={[Autoplay]}
+							autoplay={{ delay: 2500, disableOnInteraction: false }}
+							spaceBetween={30}
+							slidesPerView={6}
+							loop={true}
+						>
+							{Object.keys(productsByBrand).map(brandName => {
+								const brand = productsByBrand[brandName][0].brand;
+								return (
+									<SwiperSlide key={brandName}>
+										<div className='flex justify-center'>
+											<div className='w-32 h-32 rounded-full overflow-hidden bg-white flex items-center justify-center shadow-lg'>
+												<img
+													src={`http://localhost:1337${brand.logo.url}`}
+													alt={brandName}
+													className='w-full h-full object-cover'
+												/>
+											</div>
+										</div>
+									</SwiperSlide>
+								);
+							})}
+						</Swiper>
 					</div>
 				</div>
 			</div>
---
+++ front/src/swiper.ts
@@ -0,0 +1,5 @@
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
+export { Autoplay, Swiper, SwiperSlide };
